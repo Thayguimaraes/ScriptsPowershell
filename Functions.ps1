fc83d339d72978b6306c89aa28ea9b724e661d62
Leonardo Henrique Guimaraes <leonardo.guimaraes20@icloud.com>
2021-04-30 14:20:01 -0300

    updates

--- Functions.ps1
+++ Functions.ps1
@@ -1,9 +1,8 @@
 
 function Copy-ADGroupFromMirror {
-    param (
-        $OptionalParameters
-    )
-    
+    param ()
+    Set-ExecutionPolicy Unrestricted
+
     $MirrorId = Read-Host -Prompt 'Digite o DE espelho: '
     $DesiredId = Read-Host -Prompt 'Digite o DE de destino: '
 
@@ -78,11 +77,40 @@ function hihi{
     }
 }
 
-<#
-# Add a parameter called username.
- param (
-     $username
- )
+function Connect-office365{
+    $credential = Get-Credential
+    $urlOutlook = "https://ps.outlook.com/powershell"
+
+    $Session = New-PSSession ` 
+            -ConfigurationName Microsoft.Exchange `
+            -ConnectionUri  $urlOutlook `
+            -Credential $credential `
+            -Authentication Basic `
+            -AllowRedirection
+    
+    Import-module msonline
+    Connect-MsolService -Credential $credential
+}
+
+function Get-TargetResource ($Path) {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param ()
+
+    Import-Csv -Path $Path | ForEach-Object { 
+        New-Msoluser -UserPrincipalName $_.UserPrincipalName 
+                     -FirstName $_.FirstName -LastName $_.LastName
+                     -Department $_.Department -Title $_.Title 
+                     -Office $_.Office -PhoneNumber $_.PhoneNumber
+                     -Fax $_.Fax -StreetAddress $_.StreetAddress
+                     -MobilePhone $_.MobilePhone -City $_.City
+                     -State $_.State -Country $_.Country 
+                     -DisplayName $_.DisplayName -PostalCode $_.PostalCode
+                     -UsageLocation ""
+    }
+}
+
 
  # If the user did not provide a username value, show a message and exit the script.
  if (-not($username)) {
